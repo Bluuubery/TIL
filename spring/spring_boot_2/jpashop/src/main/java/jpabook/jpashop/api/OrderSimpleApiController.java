cc4ad5215ca7d76f3bbe0d69015d4846785e2ffb
Bluuubery <zephyr72836@gmail.com>
2023-01-25 09:08:30 +0900

    0124

---
+++ spring/spring_boot_2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        return all;
+    }
+
+}
---
+++ spring/springdatajpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,36 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+//        Optional<Member> byId = memberRepository.findById(savedMember.getId());
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+
+
+
+
+    }
+}
