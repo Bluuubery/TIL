461ca8cd3d41c1cf3495502f9a50db0d155d7711
Bluuubery <zephyr72836@gmail.com>
2023-05-06 23:26:46 +0900

    0506

---
+++ typescript/02/src/index.ts
@@ -0,0 +1,69 @@
+// class User {
+
+//     public email: string // 기본은 public이다
+//     #name: string // 이렇게 자스처럼 #을 달수도 있는데 pirvate이 더 낫다고 하는듯
+//     private readonly city: string = "Seoul" // private을 걸면 외부에서 접근이 안된다
+
+//     constructor(email: string, name: string) {
+//         this.email = email
+//         this.#name = name
+//     }
+
+// }
+
+class User {
+
+    protected _courseCount = 1
+    private readonly city: string = "Seoul" // private을 걸면 외부에서 접근이 안된다
+
+    // 이렇게 생성자에 넣을 수도 있음
+    constructor(
+        public email: string,
+        public name: string,
+        // private userId: string
+    ){}
+
+    // getter
+    get getAppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    // private 필드에 getter setter 설정하기
+    get courseCount(): number{
+        return this._courseCount
+    }
+
+    // setter는 반환 타입이 없다
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count shoud be more than 1")
+        }
+        this._courseCount = courseNum
+    }
+
+    // privaet 메서드 생성
+    private deleteToken(){
+        console.log("delete token");
+        
+    }
+
+
+}
+
+class Subuser extends User{
+
+    // private은 안 넘어온다
+    // protected으로 바꿔주면 상속 받은 클래스에서도 접근 가능함
+    changecourseCount(){
+        this._courseCount = 4
+    }
+
+    isFamily: boolean = true
+}
+
+
+
+const sunjun = new User("s@s.com", "sunjun")
+
+
+
---
+++ typescript/02/src/second.ts
@@ -0,0 +1,29 @@
+// interface왜 씀? -> ios 개발 환경의 사례로 설명해준다는데 =,,,
+
+interface TakePhoto {
+    cameraMode: string
+    filter: string
+    burst: number
+}
+
+// 이렇게 구현체한테 필요로 하는 기본적인 얘들을 강제할 수 있음(대충 자바랑 비슷한듯)
+// 코드 전체에 일관성을 부여한다
+class Instagram implements TakePhoto {
+
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number
+    ){}
+
+}
+
+class Youtube implements TakePhoto {
+
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number,
+        public short: string // 이렇게 구현체에서 필요한 녀석들을 더 넣을 수 있음
+    ){}
+}
